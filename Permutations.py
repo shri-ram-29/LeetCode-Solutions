031d121f22681504fc65db5ea43b7155dc69bb56
Shriram <ashriram29@gmail.com>
2023-04-27 19:04:12 +0530

    Permutation Code

---
+++ Permutations.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permute(self, l: List[int]) -> List[List[int]]:
+        def dfs(path, used, res):
+            if len(path) == len(l):
+                res.append(path[:])
+                return
+
+            for i, letter in enumerate(l):
+                if used[i]:
+                    continue
+                path.append(letter)
+                used[i] = True
+                dfs(path, used, res)
+                path.pop()
+                used[i] = False
+            
+        res = []
+        dfs([], [False] * len(l), res)
+        return res
