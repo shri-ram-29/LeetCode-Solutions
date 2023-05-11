fc3749422a9c04e71aed19dbec02bbed5adf842f
Shriram <ashriram29@gmail.com>
2023-05-11 22:50:10 +0530

    Reshape the Matrix Code

---
+++ Reshape_the_Matrix.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        if not mat:
+            return mat
+        if len(mat) * len(mat[0]) != r*c:
+            return mat
+        output = [[0 for i in range(c)] for i in range(r)]
+        idx = 0
+        while idx<r*c:
+            output[idx // c][ idx % c] =  mat[idx // len(mat[0])][idx % len(mat[0])]
+            idx += 1
+        return output
+        
