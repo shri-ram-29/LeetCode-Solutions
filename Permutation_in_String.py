cd9ba2dd74cc09e4edf20a206578c98d9bae8935
Shriram <ashriram29@gmail.com>
2023-05-11 22:51:43 +0530

    Permutation in String Code

---
+++ Permutation_in_String.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        cntr=Counter(s1)
+        w=len(s1)
+        match=0     
+
+        for i in range(len(s2)):
+            if s2[i] in cntr:
+                if not cntr[s2[i]]: match -= 1
+                cntr[s2[i]] -= 1
+                if not cntr[s2[i]]: match += 1
+
+            if i >= w and s2[i-w] in cntr:
+                if not cntr[s2[i-w]]: match -= 1
+                cntr[s2[i-w]] += 1
+                if not cntr[s2[i-w]]: match += 1
+
+            if match == len(cntr):
+                return True
+
+        return False
