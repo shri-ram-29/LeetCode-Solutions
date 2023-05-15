dada3d22bc82690b9e5ec8d336fab78fbd5c5f31
Shriram <ashriram29@gmail.com>
2023-05-15 20:52:19 +0530

    Monotonic Array Code

---
+++ Monotonic_Array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        x,y = [],[]
+        x.extend(nums),y.extend(nums)
+        x.sort()
+        y.sort(reverse=True)
+        if(x==nums or y==nums):
+            return True
+        return False
