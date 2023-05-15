02b9cb29cf96f4f170ee307eea7cef10b0e1efcb
Shriram <ashriram29@gmail.com>
2023-05-15 20:55:21 +0530

    Verifying an Alien Dictionary Code

---
+++ Verifying_an_Alien_Dictionary.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        alpha = {order[i]: i for i in range(len(order))}
+        for i in range(1,len(words)):
+            a,b = words[i-1],words[i]
+            for j in range(len(a)):
+                if j == len(b): return False
+                achar,bchar = a[j],b[j]
+                aix, bix = alpha[achar], alpha[bchar]
+                if aix<bix: break
+                if aix>bix: return False
+        return True
