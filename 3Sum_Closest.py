a3d0c21f171f5b05b0092a77a6c8b87cee797284
Shriram <ashriram29@gmail.com>
2023-06-02 10:30:45 +0530

    3Sum Closest Code

---
+++ 3Sum_Closest.py
@@ -0,0 +1,25 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        n = len(nums)
+        closest_sum = float('inf')
+
+        for i in range(n):
+            left = i + 1
+            right = n - 1
+
+            while left < right:
+                curr_sum = nums[i] + nums[left] + nums[right]
+
+                if curr_sum == target:
+                    return target
+
+                if abs(curr_sum - target) < abs(closest_sum - target):
+                    closest_sum = curr_sum
+
+                if curr_sum < target:
+                    left += 1
+                else:
+                    right -= 1
+
+        return closest_sum
