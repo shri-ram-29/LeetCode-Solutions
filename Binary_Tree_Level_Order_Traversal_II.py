7da0482b2a8134b5e977b6bfb78f4a7a29d1e3b4
shri-ram-29 <ashriram29@gmail.com>
2023-07-10 20:19:18 +0530

    Binary Tree Level Order Traversal II Code

---
+++ Binary_Tree_Level_Order_Traversal_II.py
@@ -0,0 +1,25 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        queue = []
+        result = []
+        queue.append(root)
+        while queue:
+            n = len(queue)
+            level = []
+            for _ in range(n):
+                node = queue.pop(0)
+                level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(level)
+        return result[::-1]
